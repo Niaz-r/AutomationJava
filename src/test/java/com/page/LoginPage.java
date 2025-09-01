60abaa8890a849c37bbf1f8a0853a3f1d72c8615
Niaz <niaz.ju.cse@gmail.com>
2025-09-01 21:31:54 +0600

    reportAndScreenshots

--- src/test/java/com/page/LoginPage.java
+++ src/test/java/com/page/LoginPage.java
@@ -1,27 +1,36 @@
 package com.page;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.util.Assert;
 import com.driver.PageDriver;
 import com.sun.org.apache.xerces.internal.impl.xpath.XPath;
 import com.utility.CommonMethods;
+import com.utility.GetScreenShots;
 
 import net.bytebuddy.asm.Advice.This;
 
 public class LoginPage extends CommonMethods {
 	
 	
+	ExtentTest test;
+	
+	
 	// PageFactory initialize
 	// Page locator
 	//funtionalityeded
 	
-	public LoginPage()
+	public LoginPage(ExtentTest test)
 	{
 		PageFactory.initElements(PageDriver.getCurrentDriver(),this);
-		
+		this.test =test;
 		
 	}
 	
@@ -47,19 +56,31 @@ public class LoginPage extends CommonMethods {
 		WebElement submitBtn;
 	
 	
-	public void login() {
+	public void login() throws InterruptedException, Exception {
 		
 		try {
 			
 			if(firstName.isDisplayed()) {
 				firstName.sendKeys("Niaz");
 				sleep(2000);
-				
-				
+				test.pass("test.pass(\"<p style=\\\"color:#28A745; font-size:13px; font-weight:bold;\\\"><b>First Name is found successfully!</b></p>\");\n");
+				   String screenshotPath = GetScreenShots.capture(PageDriver.getCurrentDriver(), "FirstName");
+			        String dest = System.getProperty("user.dir") + "\\ScreenShots\\" + "FirstName.png";
+			        test.pass("Screenshot:", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
 			}
 			
 		} catch (Exception e) {
-			System.out.println(e);
+			  test.fail("<p style=\"color:#FF3535; font-size:13px\"><b>First Name is not founded " + e.getMessage() + "</b></p>");
+			  
+			  Throwable t= new InterruptedException("Exception");
+			  test.fail(t);
+			  @SuppressWarnings("unused")
+			  
+			    String screenshotPath = GetScreenShots.capture(PageDriver.getCurrentDriver(), "FirstName");
+		        String dest = System.getProperty("user.dir") + "\\ScreenShots\\" + "FirstName.png";
+			    test.fail("Screenshot:", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+			 //   Assert.class (firstName.isDisplayed());
+			    PageDriver.getCurrentDriver().quit();
 		}
 		
 		try {
@@ -67,11 +88,26 @@ public class LoginPage extends CommonMethods {
 				
 				lastName.sendKeys("Rahaman");
 				sleep(2000);
+				test.pass("test.pass(\"<p style=\\\"color:#28A745; font-size:13px; font-weight:bold;\\\"><b>Lastt Name is found successfully!</b></p>\");\n");
+				  String screenshotPath = GetScreenShots.capture(PageDriver.getCurrentDriver(), "LastName");
+			        String dest = System.getProperty("user.dir") + "\\ScreenShots\\" + "LastName.png";
+				    test.pass("Screenshot:", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+				 
 			}
 			
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.out.println(e);
+			test.fail("<p style=\"color:#FF3535; font-size:13px\"><b>Last is not founded " + e.getMessage() + "</b></p>");
+			  
+			  Throwable t= new InterruptedException("Exception");
+			  test.fail(t);
+			  @SuppressWarnings("unused")
+			  
+			    String screenshotPath = GetScreenShots.capture(PageDriver.getCurrentDriver(), "LastName");
+		        String dest = System.getProperty("user.dir") + "\\ScreenShots\\" + "LastName.png";
+			    test.fail("Screenshot:", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+			 //   Assert.class (firstName.isDisplayed());
+			    PageDriver.getCurrentDriver().quit();
 		}
 		
 		try {
@@ -79,11 +115,23 @@ public class LoginPage extends CommonMethods {
 			{
 				submitBtn.click();
 				sleep(2000);
-				
+				   String screenshotPath = GetScreenShots.capture(PageDriver.getCurrentDriver(), "submitbatton");
+			        String dest = System.getProperty("user.dir") + "\\ScreenShots\\" + "submitbutton.png";
+				    test.pass("Screenshot:", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
-			System.out.println(e);
+			test.fail("<p style=\"color:#FF3535; font-size:13px\"><b>Submit Button Name is not founded " + e.getMessage() + "</b></p>");
+			  
+			  Throwable t= new InterruptedException("Exception");
+			  test.fail(t);
+			  @SuppressWarnings("unused")
+			  
+			    String screenshotPath = GetScreenShots.capture(PageDriver.getCurrentDriver(), "submitbatton");
+		        String dest = System.getProperty("user.dir") + "\\ScreenShots\\" + "submitbutton.png";
+			    test.fail("Screenshot:", MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+			 //   Assert.class (firstName.isDisplayed());
+			    PageDriver.getCurrentDriver().quit();
 		}
 		
 	}
--- src/test/java/com/test/LoginTest.java
+++ src/test/java/com/test/LoginTest.java
@@ -1,13 +1,21 @@
 package com.test;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.driver.BaseDriver;
 import com.driver.PageDriver;
 import com.page.LoginPage;
+import com.utility.ExtentFactory;
 
 public class LoginTest extends BaseDriver{
+	
+	
+	ExtentReports report;
+	ExtentTest parentTest,childTest;
 	
 	@BeforeClass
 	public void openUrl() throws InterruptedException {
@@ -16,16 +24,28 @@ public class LoginTest extends BaseDriver{
 		PageDriver.getCurrentDriver().get(url);
 	//	Thread.sleep(5000);
 		
+		report = ExtentFactory.getInstance();
+		parentTest = report.createTest("<span style=\\\"color:#1E90FF; font-size:16px; font-weight:bold;\\\">Main Test Title</span>");
+		
+		
 		
 	}
 	
 	@Test
-	public void loginTest() {
+	public void loginTest() throws InterruptedException, Exception {
 		
-		LoginPage loginPage = new LoginPage();
+		childTest = parentTest.createNode("<span style=\"color:#FF8C00; font-size:14px; font-weight:bold;\">Login Test</span>");
+
+		
+		LoginPage loginPage = new LoginPage(childTest);
 		loginPage.login();
 		
 		
 	}
+	@AfterClass
+	public void afterClass() {
+		report.flush();
+	}
+	
 
 }
